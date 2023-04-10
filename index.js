80e73ae829ff1d69fd45f5d981cff1428983b1a1
MatheusKristman <kristman058@gmail.com>
2023-04-10 17:59:21 -0300

    testing post route

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-//app.use(helmet());
-//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-//app.use(morgan("common"));
-app.use(express.json({ limit: "30mb", extended: false }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));

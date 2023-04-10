52b311b0af931aa639fa4274c672c29ffbf5928f
MatheusKristman <kristman058@gmail.com>
2023-04-10 17:38:44 -0300

    teste helmet

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+//app.use(helmet());
+//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));

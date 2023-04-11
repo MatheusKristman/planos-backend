69eb10916b6fd94e4b3795abdfc649f3ca671379
MatheusKristman <kristman058@gmail.com>
2023-04-11 10:21:51 -0300

    another body test

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: false }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 

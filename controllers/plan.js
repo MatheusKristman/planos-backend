b57a8de13f46cbec65582ee8c0535c53beee0242
MatheusKristman <kristman058@gmail.com>
2023-04-12 13:38:19 -0300

    filter route added

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,121 +1,161 @@
-import Plan from "../models/Plan.js";
+import Plan from '../models/Plan.js';
 
 export const getAll = async (req, res) => {
-  try {
-    const plans = await Plan.find();
+    try {
+        const plans = await Plan.find();
 
-    return res.status(200).json({ plans: plans });
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
-  }
+        return res.status(200).json({ plans: plans });
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
 };
 
 export const createPlan = async (req, res) => {
-  try {
-    const {
-      city,
-      provider,
-      title,
-      cost,
-      period,
-      franchise,
-      unlimitedApps,
-      unlimitedCall,
-      planType,
-      priority,
-      description,
-      lines,
-    } = req.body;
+    try {
+        const {
+            city,
+            provider,
+            title,
+            cost,
+            period,
+            franchise,
+            unlimitedApps,
+            unlimitedCall,
+            planType,
+            priority,
+            description,
+            lines,
+        } = req.body;
 
-    const planSelected = await Plan.findOne({ title });
+        const planSelected = await Plan.findOne({ title });
 
-    if (planSelected) {
-      return res.status(405).json({ message: "Plano já criado" });
+        if (planSelected) {
+            return res.status(405).json({ message: 'Plano já criado' });
+        }
+
+        const newPlan = new Plan({
+            city,
+            provider,
+            providerLogo: req.file.filename,
+            title,
+            cost,
+            period,
+            franchise,
+            unlimitedApps,
+            unlimitedCall,
+            planType,
+            priority,
+            description,
+            lines,
+        });
+
+        await newPlan.save();
+
+        const plans = await Plan.find();
+
+        return res.status(200).json(plans);
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
     }
-
-    const newPlan = new Plan({
-      city,
-      provider,
-      providerLogo: req.file.filename,
-      title,
-      cost,
-      period,
-      franchise,
-      unlimitedApps,
-      unlimitedCall,
-      planType,
-      priority,
-      description,
-      lines,
-    });
-
-    await newPlan.save();
-
-    const plans = await Plan.find();
-
-    return res.status(200).json(plans);
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
-  }
 };
 
 export const toggleArchivatedPlan = async (req, res) => {
-  try {
-    const { id } = req.body;
-    const planSelected = await Plan.findOne({ _id: id });
+    try {
+        const { id } = req.body;
+        const planSelected = await Plan.findOne({ _id: id });
 
-    if (!planSelected) {
-      return res.status(404).json({ message: "Plano não encontrado" });
+        if (!planSelected) {
+            return res.status(404).json({ message: 'Plano não encontrado' });
+        }
+
+        planSelected.archived = !planSelected.archived;
+
+        await planSelected.save();
+
+        const plans = await Plan.find();
+
+        return res.status(200).json(plans);
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
     }
-
-    planSelected.archived = !planSelected.archived;
-
-    await planSelected.save();
-
-    const plans = await Plan.find();
-
-    return res.status(200).json(plans);
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
-  }
 };
 
 export const editPlan = async (req, res) => {
-  try {
-    const {
-      id,
-      title,
-      cost,
-      period,
-      franchise,
-      unlimitedApps,
-      unlimitedCall,
-      planType,
-      priority,
-      description,
-      lines,
-    } = req.body;
+    try {
+        const {
+            id,
+            title,
+            cost,
+            period,
+            franchise,
+            unlimitedApps,
+            unlimitedCall,
+            planType,
+            priority,
+            description,
+            lines,
+        } = req.body;
 
-    await Plan.findOneAndUpdate(
-      { _id: id },
-      {
-        title,
-        cost,
-        period,
-        franchise,
-        unlimitedApps,
-        unlimitedCall,
-        planType,
-        priority,
-        description,
-        lines,
-      }
-    );
+        await Plan.findOneAndUpdate(
+            { _id: id },
+            {
+                title,
+                cost,
+                period,
+                franchise,
+                unlimitedApps,
+                unlimitedCall,
+                planType,
+                priority,
+                description,
+                lines,
+            }
+        );
 
-    const plans = await Plan.find();
+        const plans = await Plan.find();
 
-    return res.status(200).json(plans);
-  } catch (err) {
-    return res.status(400).json({ message: err.message });
-  }
+        return res.status(200).json(plans);
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+};
+
+export const filterPlan = async (req, res) => {
+    try {
+        const {
+            city,
+            lines,
+            cost,
+            franchise,
+            provider,
+            planType,
+            unlimitedApps,
+        } = req.body;
+
+        const plans = await Plan.find({
+            city,
+            lines,
+            cost: { $lt: cost + 1 },
+            franchise: { $lt: franchise + 1 },
+        });
+
+        console.log(plans[0].unlimitedApps.includes(unlimitedApps));
+
+        const planFiltered = plans.filter((plan) => {
+            if (
+                provider.includes(plan.provider) &&
+                planType.includes(plan.planType)
+            ) {
+                return plan.unlimitedApps.filter((el) => {
+                    if (unlimitedApps.includes(el)) {
+                        return plan;
+                    }
+                });
+            }
+        });
+
+        return res.status(200).json(planFiltered);
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
 };
--- routes/plan.js
+++ routes/plan.js
@@ -1,10 +1,16 @@
-import express from "express";
-import { getAll, toggleArchivatedPlan, editPlan } from "../controllers/plan.js";
+import express from 'express';
+import {
+    getAll,
+    toggleArchivatedPlan,
+    editPlan,
+    filterPlan,
+} from '../controllers/plan.js';
 
 const router = express.Router();
 
-router.get("/all", getAll);
-router.put("/archive", toggleArchivatedPlan);
-router.put("/edit", editPlan);
+router.get('/all', getAll);
+router.put('/archive', toggleArchivatedPlan);
+router.put('/edit', editPlan);
+router.post('/filter', filterPlan);
 
 export default router;

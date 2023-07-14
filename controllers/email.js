36e6e4373ed1460bef8f8989a1e6b9c4bd8c25bc
MatheusKristman <kristman058@gmail.com>
2023-07-14 13:20:27 -0300

    ready to put email function

--- controllers/email.js
+++ controllers/email.js
@@ -1,1 +1,34 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const smtp = nodemailer.createTransport({
+  host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
+  secure: true,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
+
+const configEmail = {
+  from: "kristman058@gmail.com",
+  to: process.env.NODEMAILER_USER,
+  subject: "Email teste enviado do nodemailer",
+  html: `<p>Email teste enviado do nodemailer</p>`,
+};
+
+export const sendEmail = async (req, res) => {
+  smtp
+    .sendMail(configEmail)
+    .then((res) => {
+      smtp.close();
+      return res.status(200).json({ message: "Email enviado com sucesso" });
+    })
+    .catch((error) => {
+      smtp.close();
+      return res.status(400).json({ message: error.message });
+    });
+};
